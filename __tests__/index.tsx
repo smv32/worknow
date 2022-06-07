80226b38120978bd794af05f0bbe9a753bf61c63
Samarth Verma <samarthverma@icloud.com>
2022-06-07 21:44:11 +0530

    Format code and remove unused api functionality

--- __tests__/index.tsx
+++ __tests__/index.tsx
@@ -1,7 +1,9 @@
-import { render ,screen} from '@testing-library/react'
-import Home from '../pages/index'
-import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react';
+import Home from '../pages/index';
+import '@testing-library/jest-dom';
 it('renders Hello World in the center of the page', () => {
-    render(<Home />)
-    expect(screen.getByText('Hello World')).toHaveClass('grid place-items-center')
-})
+  render(<Home />);
+  expect(screen.getByText('Hello World')).toHaveClass(
+    'grid place-items-center'
+  );
+});
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-const nextJest = require('next/jest')
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   dir: __dirname
-})
+});
 
 module.exports = createJestConfig({
-    testEnvironment: 'jsdom'
-})
+  testEnvironment: 'jsdom'
+});
