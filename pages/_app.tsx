a746ece5b2d70019e5ee8ee881e2ee924a085278
Samarth Verma <samarthverma@icloud.com>
2022-06-07 19:14:01 +0530

    Configure prettier for formatting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import type { AppProps } from 'next/app'
-import 'tailwindcss/tailwind.css'
+import type { AppProps } from 'next/app';
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
-  return <div className="min-h-screen bg-zinc-50 place-items-center grid text-2xl">Hello World</div>;
+  return (
+    <div className="grid min-h-screen place-items-center bg-zinc-50 text-2xl">
+      Hello World
+    </div>
+  );
 };
 
 export default Home;
