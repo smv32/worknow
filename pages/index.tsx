cf06f9dcdb97227c598acd8e873bb7334ddd6e4a
Samarth Verma <samarthverma@icloud.com>
2022-06-24 16:39:12 +0530

    extract navigation menu component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
 
-import clsx from 'clsx';
 import { HiCheck, HiLocationMarker } from 'react-icons/hi';
 import supabase from '../helpers/supabase';
 import { useEffect, useState } from 'react';
@@ -11,55 +8,9 @@ import { formatDistance } from 'date-fns';
 
 import * as Checkbox from '@radix-ui/react-checkbox'
 import * as Label from '@radix-ui/react-label'
+import NavigationMenu from '../components/NavigationMenu';
 
-const links = ['Home', 'Jobs', 'Resources'];
 
-const Navbar = () => {
-  return (
-    <nav className="bg-white px-4 py-3">
-      <div className="container mx-auto flex justify-between border-b">
-        <a href="https://flowbite.com/" className="flex items-center py-2 pb-4">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="mr-3 h-6 sm:h-9"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            Flowbite
-          </span>
-        </a>
-
-        <div>
-          <ul className="flex h-12 items-center gap-2">
-            {links.map((link, idx) => (
-              <li key={idx}>
-                <Link href="/">
-                  <a
-                    className={clsx(
-                      'rounded-lg px-3 py-2 text-slate-600',
-                      idx == 1 && 'bg-zinc-100 text-slate-800',
-                      'transition-colors hover:text-slate-900'
-                    )}
-                  >
-                    {link}
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="flex h-12 items-center gap-8">
-          <button className="text-sm font-semibold tracking-wide">
-            Sign In
-          </button>
-          <button className="appearance-none rounded bg-blue-600 px-4 py-2 text-sm font-semibold tracking-wide text-white transition-colors hover:bg-blue-700">
-            Create Account
-          </button>
-        </div>
-      </div>
-    </nav>
-  );
-};
 
 function Filter() {
   return (
@@ -94,7 +45,7 @@ const Home: NextPage = () => {
         <title>Home | WorkNow</title>
       </Head>
       <div className="min-h-screen w-full">
-        <Navbar />
+        <NavigationMenu />
         <div className="container mx-auto flex gap-8">
           <Content />
           <Filter />
