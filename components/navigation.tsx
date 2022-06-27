3d6696d9f02a4398cf2e1d21fc1cb4652e7da37d
Samarth Verma <samarthverma@icloud.com>
2022-06-27 15:46:44 +0530

    Update homeage navigation menu

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,7 +1,12 @@
-/* This example requires Tailwind CSS v2.0+ */
 import { Disclosure } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import * as Avatar from '@radix-ui/react-avatar';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import initials from 'initials';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import supabase from '../helpers/supabase';
+import useUser from '../hooks/useUser';
 
 const navigation = [
   { name: 'Home', href: '#', current: true },
@@ -9,6 +14,7 @@ const navigation = [
   { name: 'Get Resources', href: '#', current: false }
 ];
 
+// @ts-ignore
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
@@ -57,11 +63,12 @@ export default function Header() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <Link href='/login'>
+                {/* <Link href='/login'>
                   <button className="ml-2 rounded bg-gray-800 px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
                     Sign in
                   </button>
-                </Link>
+                </Link> */}
+                <UserStatus />
               </div>
             </div>
           </div>
@@ -89,5 +96,69 @@ export default function Header() {
         </>
       )}
     </Disclosure>
+  );
+}
+
+const UserProfile = ({ name }: { name: string }) => {
+  return (
+    <Avatar.Root className="inline-flex h-10 w-10 cursor-pointer select-none items-center justify-center rounded-full bg-black">
+      <Avatar.Fallback className="flex h-full w-full items-center justify-center rounded-full bg-gradient-to-r from-rose-100 to-teal-100 font-medium text-gray-900 ring-2 ring-gray-100 ring-offset-2">
+        {initials(name)}
+      </Avatar.Fallback>
+    </Avatar.Root>
+  );
+};
+
+function UserStatus() {
+  const { isLoading, user } = useUser();
+  const router = useRouter();
+
+  if (isLoading) {
+    return null;
+  }
+  if (!user) {
+    return (
+      <div className="flex h-12 items-center gap-8">
+        <Link href={'/login'}>
+          <button className="text-sm font-semibold text-slate-50 tracking-wide">
+            Sign In
+          </button>
+        </Link>
+        <Link href={'/signup'}>
+          <button className="appearance-none rounded-lg bg-zinc-50 px-4 py-2 text-sm font-semibold tracking-wide text-blue-600 transition-colors hover:bg-zinc-200">
+            Create Account
+          </button>
+        </Link>
+      </div>
+    );
+  }
+
+  const name = user.user_metadata.full_name;
+
+  return (
+    <div>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <UserProfile name={name || 'Z'} />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content
+          className="z-50 w-40 rounded-xl border bg-white shadow-lg"
+          align="end"
+          sideOffset={12}
+        >
+          <DropdownMenu.Item>
+            <button
+              className="w-full px-4 py-2 text-left text-slate-500 hover:bg-gray-100"
+              onClick={() => {
+                supabase.auth.signOut();
+                router.push('/');
+              }}
+            >
+              Sign Out
+            </button>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </div>
   );
 }
